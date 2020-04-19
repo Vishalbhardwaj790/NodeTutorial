f5ca9c8ad7363f75968168c5c3fe6c44aef7a0a5
Vishal Sharma <vishal.bhardwaj790@gmail.com>
2020-04-19 21:46:26 +0530

    Port Changes in App.js

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const path = require("path");
 const app = express();
 
 const port  = process.env.PORT || 8000
-
+app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res.render("index.hbs", { title: 'Template', message: 'Hello from Heroku' });
 });
+
 
 // app.get("/",(req,res)=>{
 //   res.send("Hello")
